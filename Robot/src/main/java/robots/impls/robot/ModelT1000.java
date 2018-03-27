e0a03939a2d8d7779675ab49a265ef9035c22470
Andrei Fedotov <andrei.fedotov@netcracker.com>
2018-03-27 11:01:51 +0300

    Getters and setters and p namespace. Lesson 9. Order of parameters.

--- Robot/src/main/java/robots/impls/robot/ModelT1000.java
+++ Robot/src/main/java/robots/impls/robot/ModelT1000.java
@@ -11,6 +11,34 @@ public class ModelT1000 implements Robot{
     private Leg leg;
     private Head head;
 
+    private String color;
+    private int year;
+    private boolean soundEnabled;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
+    }
+
     public Hand getHand() {
         return hand;
     }
@@ -45,11 +73,29 @@ public class ModelT1000 implements Robot{
 
     }
 
+    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
     @Override
     public void fire() {
         head.calc();
         hand.catchSomething();
         leg.go();
+        System.out.println("color: "  + getColor());
+        System.out.println("year: "  + getYear());
+        System.out.println("soundEnabled: "  + isSoundEnabled());
     }
 
     @Override
